584bd84c95541d7f3a72b31f29a3d77aed34bc69
Hugo Silva <jose.silva@extreme.digital>
2024-11-29 17:43:42 -0300

    feat: base64

--- src/main/java/io/github/yhugorocha/newFeaturesJava8/base64/Exemplo01.java
+++ src/main/java/io/github/yhugorocha/newFeaturesJava8/base64/Exemplo01.java
@@ -6,6 +6,8 @@ import java.util.Base64;
 public class Exemplo01 {
     public static void main(String[] args) {
 
+        //Esta classe oferece três tipos de codificadores e decodificadores para os grupos de dados: básico (alfabeto), URL e MIME.
+
         String texto = "A classe base64 no java 8";
         String textoCodificado = Base64.getEncoder().encodeToString(texto.getBytes(StandardCharsets.UTF_8));
         String textoDecodificado = new String(Base64.getDecoder().decode(textoCodificado), StandardCharsets.UTF_8);
